8f85c2c79e4345e5ca4e81def1bc00bbc50ed73e
AVBr0 <logweb01@gmail.com>
2022-02-25 02:44:12 +0300

    morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let finDateArray = [];
+
+    // Деление по десяткам
+    for (let i = 0; i < expr.length; i += 10)  {
+        const decimalPart = expr.slice(i, i + 10);
+
+        //Обрезка нулей перед искомым значением
+        const withoutNullPart = decimalPart.slice(decimalPart.indexOf('1'));
+        let resultString = '';
+            for (let i = 0; i < withoutNullPart.length; i += 2) {  // Начало перевода 10,11,* в ключи словаря
+                const binaryPart = withoutNullPart.slice(i, i + 2);  
+                    switch (binaryPart) {
+
+                    case '10':
+                    resultString += '.';
+                    break;
+
+                    case '11':
+                    resultString += '-';
+                    break;
+
+                    case '*':
+                    resultString += ' ';
+                    break;
+                 }
+             }
+      finDateArray.push(resultString);
+    }
+
+    // Возвращение финального значения после сравнения со словарем
+    return finDateArray.map((key) => (key === ' ' ? ' ' : MORSE_TABLE[key])).join('');
 }
 
 module.exports = {
